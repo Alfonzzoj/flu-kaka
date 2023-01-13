90144fc45e0b618b6638170324c44dd8662ee948
Jesus Alfonzo <jesusalfonzo97@gmail.com>
2023-01-13 11:35:36 -0400

    Parte estatica lista

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,14 @@ router.get('/', (req, res) => res.render('index'))
 router.get('/login', (req, res) => res.render('login'))
 router.get('/registro', (req, res) => res.render('registro'))
 router.get('/dashboard', (req, res) => res.render('dashboard'))
-router.get('/pedir/seleccion_zona', (req, res) => res.render('seleccion_zona'))
-router.get('/pedir/seleccion_zona/colas_en_zona', (req, res) => res.render('colas_en_zona'))
+router.get('/pedir/seleccion_zona', (req, res) => res.render('pedir_seleccion_zona'))
+router.get('/pedir/seleccion_zona/colas_en_zona', (req, res) => res.render('pedir_colas_en_zona'))
+router.get('/dar/seleccion_zona', (req, res) => res.render('dar_seleccion_zona'))
+router.get('/dar/gestion_de_encuentro', (req, res) => res.render('gestion_de_encuentro'))
+router.get('/dar/solicitudes_para_viaje', (req, res) => res.render('solicitudes_para_viaje'))
+// router.get('/dar/seleccion_zona/colas_en_zona', (req, res) => res.render('dar_colas_en_zona'))
+router.get('/dar_review_a_conductor', (req, res) => res.render('dar_review_a_conductor'))
+router.get('/en_viaje', (req, res) => res.render('en_viaje'))
 router.get('/profile', (req, res) => {
     // Dumbp data
     req.user = {
